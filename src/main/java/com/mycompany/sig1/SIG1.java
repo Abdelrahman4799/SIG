e1975e351e9ea5b061ac9de507fa00138b980e40
Abd El-Rahman Mohamed Mohamed <32821693+Abdelrahman4799@users.noreply.github.com>
2022-11-23 14:35:25 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/sig1/SIG1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sig1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class SIG1 {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        Frame myframe = new Frame();
+        myframe.setVisible(true);
+    }
+}
